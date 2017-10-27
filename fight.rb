9c2508545df2b63da88f197fa083962069195533
Isaac Seymour <i.seymour@oxon.org>
2017-10-27 17:20:49 +0100

    Add Jade and Pedro to fight

--- fight.rb
+++ fight.rb
@@ -2,7 +2,7 @@ require_relative './lib/tournament'
 
 Dir['./bots/*.rb'].each { |f| require f }
 
-bots = [David, Stephen, Isaac, Constant, Alex, MuyiwaBot]
+bots = [David, Stephen, Isaac, Constant, Alex, MuyiwaBot, JadeBot, Pedro]
 
 bots.combination(2).each do |bot1, bot2|
   tournament = Tournament.new(bot1.new, bot2.new)
