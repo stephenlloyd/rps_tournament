ffb6664553b048b68de22c2326163d1060706662
Isaac Seymour <i.seymour@oxon.org>
2017-10-27 17:32:10 +0100

    Show win counts

--- fight.rb
+++ fight.rb
@@ -4,6 +4,8 @@ Dir['./bots/*.rb'].each { |f| require f }
 
 bots = [David, Stephen, Isaac, Constant, Alex, MuyiwaBot, JadeBot, Pedro]
 
+win_counts = Hash.new { |hash, key| hash[key] = 0 }
+
 bots.combination(2).each do |bot1, bot2|
   tournament = Tournament.new(bot1.new, bot2.new)
   puts "Playing #{bot1.new.name} against #{bot2.new.name}"
@@ -14,5 +16,13 @@ bots.combination(2).each do |bot1, bot2|
     puts error.backtrace
   end
   puts tournament.report
+
+  win_counts[tournament.winner] += 1
   puts
 end
+
+puts
+win_counts
+  .to_a
+  .sort_by { |(name, count)| -1 * count }
+  .each_with_index { |(name, count), index| puts "#{index + 1}. #{name} won #{count} times" }
